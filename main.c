86d8a6142f2f7dd8bda18e88bf3baea730631f33
Kevin W Matthews <KevinWMatthews@gmail.com>
2019-01-11 09:13:38 -0800

    Add seek to null io example

--- main.c
+++ main.c
@@ -40,20 +40,28 @@ void null_io(void)
     }
 
     char buffer[1024] = {0};
-    size_t result = 0;
+    ssize_t count = 0;
 
     // size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream);
-    result = fread(buffer, sizeof(buffer[0]), sizeof(buffer), file);
-    if (result == 0)
+    count = fread(buffer, sizeof(buffer[0]), sizeof(buffer), file);
+    if (count == 0)
         fprintf(stderr, "Failed to read\n");
 
     // size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream);
-    result = fwrite(buffer, sizeof(buffer[0]), sizeof(buffer), file);
-    if (result == 0)
+    count = fwrite(buffer, sizeof(buffer[0]), sizeof(buffer), file);
+    if (count == 0)
         fprintf(stderr, "Failed to write\n");
 
+    int result = 0;
+
+    // int fseek(FILE *stream, long offset, int whence);
+    result = fseek(file, 0L, SEEK_SET);
+    if (result == -1)
+        fprintf(stderr, "Failed to seek\n");
+
     // int fclose(FILE *stream);
-    if (fclose(file) != 0)
+    result = fclose(file);
+    if (result != 0)
         fprintf(stderr, "Failed to close\n");
 }
 
